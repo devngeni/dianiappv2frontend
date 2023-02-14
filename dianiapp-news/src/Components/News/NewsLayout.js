20482d46ebd5c6f73c20aae520adbb666fcef717
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2023-02-14 17:20:26 +0300

    file tree restructuring

--- dianiapp-news/src/Components/News/NewsLayout.js
+++ dianiapp-news/src/Components/News/NewsLayout.js
@@ -4,10 +4,10 @@ import './News.css'
 function NewsLayout() {
   return (
     <div className='NewsLayout'>
-   <div className='news-img'><img src='/images/news.png' className='news-img-spec'/></div>
+   <div className='news-img items-center flex'><img src='/images/news.png' className='news-img-spec'/></div>
    <div className='news-content'>
-    <div className='topic'>   Avril is Coming</div>
-    <div>Lorem ipsum dolor sit amet consectetur. In vulputate ultrices eu netus condimentum commodo nulla sollicitudin. Faucibus eget amet diam feugiat at tempus enim. </div>
+    <div className='topic '>   Avril is Coming</div>
+    <div className='content'>Lorem ipsum dolor sit amet consectetur. In vulputate ultrices eu netus condimentum commodo nulla sollicitudin. Faucibus eget amet diam feugiat at tempus enim. </div>
     <div className='flex date'><p>24th Jan </p> <p> <span>.</span>10:58pm</p></div>
    </div>
     </div>
